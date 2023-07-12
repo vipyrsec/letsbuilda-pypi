73dd55df0fd8cff6f00e77ae62b562901e6f043d
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-07-12 21:53:42 +0000

    remove non-existent import
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

--- src/letsbuilda/pypi/models/__init__.py
+++ src/letsbuilda/pypi/models/__init__.py
@@ -1,5 +1,4 @@
 """Response models"""
 
 from .models_json import JSONPackageMetadata  # noqa: F401
-from .models_package import PackageMetadata  # noqa: F401
 from .models_rss import RSSPackageMetadata  # noqa: F401
