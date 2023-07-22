b34e2d036acdfac3e1e750fdfb074dfa51861120
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-07-22 01:37:45 +0000

    add imports back to init
    Signed-off-by: GitHub <noreply@github.com>

--- src/letsbuilda/pypi/models/__init__.py
+++ src/letsbuilda/pypi/models/__init__.py
@@ -1,1 +1,11 @@
 """Models to hold the data."""
+
+from .models_json import JSONPackageMetadata as JSONPackageMetadata
+from .models_package import Package as Package
+from .models_rss import RSSPackageMetadata as RSSPackageMetadata
+
+__all__ = [
+    "Package",
+    "JSONPackageMetadata",
+    "RSSPackageMetadata",
+]
